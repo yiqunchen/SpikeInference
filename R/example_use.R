36a79e9f2786bf06f2f7285af0d8be4a099b5ab8
yiqunchen <yiqunchen1995@gmail.com>
2020-06-04 22:59:33 -0700

    fixing again

--- R/example_use.R
+++
@@ -1,6 +0,0 @@
-
-
-sim <- simulate_ar1(n = 10000, gam = 0.95, poisMean = 0.01, sd = 0.15, seed = 1)
-
-spike_estimates(dat, decay_rate, tuning_parameter, functional_pruning_out = FALSE)
-spike_inference(dat, decay_rate, tuning_parameter, window_size = NULL, sig = NULL, return_conditioning_sets = FALSE)
