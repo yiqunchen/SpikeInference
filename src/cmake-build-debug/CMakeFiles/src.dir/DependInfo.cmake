71e153e90ab065073ae64a2a3cde2c39ad4ee2f6
yiqunchen <yiqunchen1995@gmail.com>
2021-01-29 16:06:30 -0800

    travis test

---
+++ src/cmake-build-debug/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/fpop.cpp" "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/cmake-build-debug/CMakeFiles/src.dir/fpop.cpp.o"
+  "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/fpop_inference.cpp" "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/cmake-build-debug/CMakeFiles/src.dir/fpop_inference.cpp.o"
+  "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/funPieceList.cpp" "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/cmake-build-debug/CMakeFiles/src.dir/funPieceList.cpp.o"
+  "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/main.cpp" "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/cmake-build-debug/CMakeFiles/src.dir/main.cpp.o"
+  "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/selective_inference.cpp" "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/cmake-build-debug/CMakeFiles/src.dir/selective_inference.cpp.o"
+  "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/utils.cpp" "/Users/tonyyiqunchen/Desktop/dissertation/ell_0_project/SpikeInference/src/cmake-build-debug/CMakeFiles/src.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- tests/testthat/test_spike_inference.R
+++ tests/testthat/test_spike_inference.R
@@ -3,8 +3,8 @@ library(SpikeInference)
 
 test_that("Spike estimation on simple examples should agree with the analytical solution", {
   expect_equal(4, spike_inference(c(3, 2.7, 2.43, 2.18, 2.7, 2.43), 
-                                0.9, 0.1)$spikes)
+                                0.9, 0.1,2)$spikes)
   expect_equal(TRUE, spike_inference(c(3, 2.7, 2.43, 2.18, 2.7, 2.43), 
-                                  0.9, 0.1)$pvals<=0.05)
+                                  0.9, 0.1,2)$pvals<=0.05)
 })
 
